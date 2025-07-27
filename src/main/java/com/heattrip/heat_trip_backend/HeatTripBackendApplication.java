d961247e62c47bc22217ff9ea6bd3e3b601fe906
HYUN <149238724+sanghyunbang@users.noreply.github.com>
2025-07-27 18:04:31 +0900

    Initial commit

---
+++ src/main/java/com/heattrip/heat_trip_backend/HeatTripBackendApplication.java
@@ -0,0 +1,13 @@
+package com.heattrip.heat_trip_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeatTripBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeatTripBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heattrip/heat_trip_backend/HeatTripBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heattrip.heat_trip_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeatTripBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
