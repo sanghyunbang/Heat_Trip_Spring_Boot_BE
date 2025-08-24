6327639bbe4a723c8a979dae1f12ba28ec7c6f88
HYUN <149238724+sanghyunbang@users.noreply.github.com>
2025-08-24 15:58:24 +0900

    [chore] S3 관련 설정

---
+++ src/main/java/com/heattrip/heat_trip_backend/S3/AmazonS3Config.java
@@ -0,0 +1,39 @@
+package com.heattrip.heat_trip_backend.S3;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+// S3에 접근 할 수 있도록 클라이언트 생성하는 부분!
+// @PropertySource("classpath:application_AWS.properties")
+@Configuration
+public class AmazonS3Config {
+    // 프로퍼티에 적어놓은 정보들 담는 부분분
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+    
+    // 이 객체를 만들어서 Spring 컨테이너에 등록하고, 이걸로 S3 접근함!
+    // 여기서 AmazonS3는 인터페이스고 리턴되는 객체는 AmazonS3Client
+    @Bean
+    public AmazonS3 amazonS3() {
+        // AWS 자격 증명 관련한 객체 생성하기
+        BasicAWSCredentials creds = new BasicAWSCredentials(accessKey, secretKey);
+        // S3 클라이언트 생성 하고 Bean 등록하기  
+        return AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(creds))
+                .build();
+    }
+}
---
+++ src/main/java/com/heattrip/heat_trip_backend/S3/S3Service.java
@@ -0,0 +1,60 @@
+package com.heattrip.heat_trip_backend.S3;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.RequiredArgsConstructor;
+
+// 여기서 실제로 S3에 파일을 올리는 역할 함!(AmasonS3Config에서 만든 객체 여기서 씀)
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    // CDN 도메인 
+    @Value("${cloud.aws.cloudfront.domain}")
+    private String cloudFrontDomain;
+    
+    // 이전 Config에서 만든 Bean이 들어감!! (RequiredArgsConstructor가 해줌)
+    private final AmazonS3 amazonS3; 
+
+    // 스프링에서 제공하는 MULTIPART 씀!! (추가 공부 해야 할듯)
+    public String uploadFile(MultipartFile file) {
+        // 파일명 안겹치게 하려고 하는 부분분
+        String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename();
+
+        try {
+            //S3에 파일정보 같이 보냄(아마존 관련 라이브러리 씀)
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentLength(file.getSize());
+
+            metadata.setContentType(file.getContentType()); // 파일의 MIME 타입을 s3에 알려줌
+
+            PutObjectRequest putRequest = new com.amazonaws.services.s3.model.PutObjectRequest(
+                bucket, fileName, file.getInputStream(), metadata
+            );
+
+            amazonS3.putObject(putRequest);
+
+        } catch (IOException e) {
+            throw new RuntimeException("S3 업로드 실패", e);
+        }
+
+        // 업로드가 성공했으면, 그 파일의 S3 URL을 반환 -> (수정) CloudFront URL 반환
+        // return amazonS3.getUrl(bucket, fileName).toString();
+        
+        return cloudFrontDomain + "/" + fileName;
+    }
+}
