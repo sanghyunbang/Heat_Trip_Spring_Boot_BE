c3c4632d2fc93613621093bf970e3c7c19ce2e86
HYUN <149238724+sanghyunbang@users.noreply.github.com>
2025-08-25 21:32:59 +0900

    [fix] Place repository 제네릭 변경

--- src/main/java/com/heattrip/heat_trip_backend/tour/repository/PlaceTraitSnapshotRepo.java
+++ src/main/java/com/heattrip/heat_trip_backend/tour/repository/PlaceTraitSnapshotRepo.java
@@ -3,4 +3,4 @@ package com.heattrip.heat_trip_backend.tour.repository;
 import com.heattrip.heat_trip_backend.tour.domain.PlaceTraitSnapshot;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PlaceTraitSnapshotRepo extends JpaRepository<PlaceTraitSnapshot, Long> {}
+public interface PlaceTraitSnapshotRepo extends JpaRepository<PlaceTraitSnapshot, String> {}
